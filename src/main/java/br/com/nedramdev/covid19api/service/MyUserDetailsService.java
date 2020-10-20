2721dc071ec39865ab9df3fda2c8912ce1c28d1d
Marden Rocha <mardenbiel@gmail.com>
2020-10-20 20:12:31 -0300

    Fix a minor issue with granted authorities

--- src/main/java/br/com/nedramdev/covid19api/service/MyUserDetailsService.java
+++ src/main/java/br/com/nedramdev/covid19api/service/MyUserDetailsService.java
@@ -34,7 +34,7 @@ public class MyUserDetailsService implements UserDetailsService {
 
         @Override
         public Collection<? extends GrantedAuthority> getAuthorities() {
-            return getRoles();
+            return this.getRoles();
         }
 
         @Override
